016d315550b2935329e027736dcde5f0f9c82e3f
User <User@DESKTOP-ADE82NT>
2023-09-18 13:39:57 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Введите второе число: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Введите третье число: ");
+        int c = int.Parse(Console.ReadLine());
+
+        Thread.CurrentThread.Name = "main";
+        GCD();
+        Thread mythread = new Thread(GCD);
+        mythread.Name = "back";
+        mythread.Start();
+
+        void GCD()
+        {
+            Random r = new Random();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine(Thread.CurrentThread.Name + " начал работу");
+        https://skysmart.ru/articles/mathematic/naibolshij-obshchij-delitel
+            Console.WriteLine(Thread.CurrentThread.Name + " закончил работу");
+            sw.Stop();
+        }
+    }
+}
